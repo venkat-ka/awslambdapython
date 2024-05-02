8ca52e2b786ae3dcc220a04640cd2b44263a6155
venkat-ka <venkat.k.2516@gmail.com>
2024-05-02 21:57:20 +0530

    [+] final update

---
+++ student-api/hello_world/app.py
@@ -0,0 +1,16 @@
+import json
+
+from flask_lambda import FlaskLambda
+
+app = FlaskLambda(__name__)
+
+@app.route('/hello')
+def index():
+    data = {
+        "message":"Hello, world"
+    }
+    return (
+        app.json.dumps(data),
+        200,
+        {'Content-Type':'application/json'}
+    )
